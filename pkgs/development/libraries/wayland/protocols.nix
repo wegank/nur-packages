bafe2c1c57be8d8140616ec27eb0f9c8ff52b3ca
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-07-29 11:16:58 +0200

    wayland-protocols: revert

--- pkgs/development/libraries/wayland/protocols.nix
+++ pkgs/development/libraries/wayland/protocols.nix
@@ -24,7 +24,7 @@ stdenv.mkDerivation rec {
     patchShebangs tests/
   ''
   # https://gitlab.freedesktop.org/wayland/wayland-protocols/-/issues/48
-  + lib.optionalString (stdenv.system == "aarch64-darwin") ''
+  + lib.optionalString stdenv.isDarwin ''
     substituteInPlace meson.build --replace "'xdg-decoration': ['v1']," ""
   '';
 
