1846a7ac88f6faba8af7214c19935895bb7d762b
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-10-03 17:38:02 +0200

    heptagon: add wrapper

--- pkgs/development/ocaml-modules/heptagon/default.nix
+++ pkgs/development/ocaml-modules/heptagon/default.nix
@@ -1,11 +1,10 @@
 { lib
 , stdenv
 , fetchFromGitLab
+, makeWrapper
 , ocamlPackages
 , ocaml
-
-, lutils
-, rdbg
+, runtimeShell
 }:
 
 stdenv.mkDerivation rec {
@@ -19,6 +18,10 @@ stdenv.mkDerivation rec {
     rev = "v${version}";
     sha256 = "sha256-b4O48MQT3Neh8a1Z5wRgS701w6XrwpsbSMprlqTT+CE=";
   };
+
+  nativeBuildInputs = [
+    makeWrapper
+  ];
 
   buildInputs = with ocamlPackages; [
     ocaml
@@ -34,11 +37,19 @@ stdenv.mkDerivation rec {
     ocamlgraph
   ];
 
+  postFixup = ''
+    mv $out/bin/hepts $out/bin/.hepts-unwrapped
+    cat > $out/bin/hepts << EOF
+    #!${runtimeShell}
+    ${ocaml}/bin/ocamlrun -I ${ocamlPackages.lablgtk}/lib/ocaml/${ocaml.version}/site-lib/lablgtk2 $out/bin/.hepts-unwrapped "\$@"
+    EOF
+    chmod +x $out/bin/hepts
+  '';
+
   meta = with lib; {
     description = "A synchronous dataflow language whose syntax and semantics is inspired from Lustre";
     homepage = "https://gitlab.inria.fr/synchrone/heptagon";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ wegank ];
-    mainProgram = "heptc";
   };
 }
