7973cc343959a599eccf557697f7f9f237a42969
Weijia Wang <9713184+wegank@users.noreply.github.com>
2025-01-02 16:01:44 +0100

    casioemu: init at 0-unstable-2024-05-17

---
+++ pkgs/by-name/ca/casioemu/package.nix
@@ -0,0 +1,53 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  meson,
+  ninja,
+  pkg-config,
+  lua5_3,
+  readline,
+  SDL2,
+  SDL2_image,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "casioemu";
+  version = "0-unstable-2024-05-17";
+
+  src = fetchFromGitHub {
+    owner = "user202729";
+    repo = "CasioEmu";
+    rev = "52938b78451ff5f2409e95b00268f4d9d973dcdf";
+    hash = "sha256-FRPVW3Z99YOFuJv8tM9aHmcxBhAM6wYlrRLZsZRLOsY=";
+  };
+
+  sourceRoot = "${finalAttrs.src.name}/emulator";
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+  ];
+
+  buildInputs = [
+    lua5_3
+    readline
+    SDL2
+    SDL2_image
+  ];
+
+  postInstall = ''
+    install -Dm755 emulator -t $out/bin
+  '';
+
+  meta = {
+    description = "An emulator for nX-U8 based Casio fx-es PLUS calculators";
+    homepage = "https://github.com/user202729/CasioEmu";
+    licences = lib.licences.gpl3Only;
+    maintainers = with lib.maintainers; [ wegank ];
+    platforms = lib.platforms.linux;
+  };
+})
