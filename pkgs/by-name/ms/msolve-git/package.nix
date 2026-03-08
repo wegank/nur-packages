5d548f5df04cdc64f824c30ea7eceba81601d30b
Weijia Wang <9713184+wegank@users.noreply.github.com>
2026-03-08 22:41:04 +0100

    msolve-git: 0.9.4-unstable-2026-02-23 -> 0.9.4-unstable-2026-03-08

--- pkgs/by-name/ms/msolve-git/package.nix
+++ pkgs/by-name/ms/msolve-git/package.nix
@@ -4,11 +4,11 @@
 }:
 
 msolve.overrideAttrs {
-  version = "0.9.4-unstable-2026-02-23";
+  version = "0.9.4-unstable-2026-03-08";
   src = fetchFromGitHub {
     owner = "algebraic-solving";
     repo = "msolve";
-    rev = "2e43f3f91c4b8c9b9116a248b0a74f4e49446b80";
-    hash = "sha256-Izq61NZOc76cnrjLKCmu1rbe9G4Rb2cvI0Hp1y1kOPc=";
+    rev = "ff6fb15ac785376a0d62f4820d5d87526a2e2e84";
+    hash = "sha256-gjiIHFGppriNgHfSPIB5WAOEO5nBLcL8NqS+K51McoM=";
   };
 }
